c1a0e1d5051aa2366fccf9b4faaadebfc17c8f38
Leonardo Carapecos <leonardocarapecosdev@gmail.com>
2023-06-21 21:42:00 -0300

    commit inicial

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    })
+    $('#cep').mask('00000-000', {
+        placeholder: '97528-000'
+    })
+
+    
+})
